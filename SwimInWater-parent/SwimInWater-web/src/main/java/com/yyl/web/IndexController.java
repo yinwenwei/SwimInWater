71be40debd091be0c3b3efed04dff34a7b6d04f9
kewei_liao <kewei_liao@163.com>
2019-01-11 12:41:34 +0800

    swagger显示类型修改

--- SwimInWater-parent/SwimInWater-web/src/main/java/com/yyl/web/IndexController.java
+++ SwimInWater-parent/SwimInWater-web/src/main/java/com/yyl/web/IndexController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 * @author lkw
 * @date 2019年1月10日 上午10:33:41
 * org.apache.tomcat.maven:tomcat7-maven-plugin:2.2:run
+* 注解
+* https://blog.csdn.net/java_yes/article/details/79183804
  */
 @Api(value="显示控制器", tags={"首页相关"})
 @Controller
@@ -27,16 +29,18 @@ public class IndexController {
 	
 	@ApiOperation(value="请求首页", notes="根据业务获取数据")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="param1",value="参数1",required=true, dataType="Integer", paramType="query"),
-		@ApiImplicitParam(name="param2",value="参数2",required=true, dataType="Integer", paramType="query"),
-		@ApiImplicitParam(name="param3",value="参数3",required=true, dataType="Integer", paramType="query"),
-		@ApiImplicitParam(name="param4",value="参数4",required=true, dataType="Integer", paramType="query")
+		@ApiImplicitParam(name="param1",value="参数1",required=true, paramType="query"),
+		@ApiImplicitParam(name="param2",value="参数2",required=true, paramType="query"),
+		@ApiImplicitParam(name="param3",value="参数3",required=true, paramType="query"),
+		@ApiImplicitParam(name="param4",value="参数4",required=true, paramType="query")
 	})
 	@RequestMapping(value = "/index", method=RequestMethod.GET)
 	public String index(Integer param1,Integer param2,
 						Integer param3,Integer param4){
 		logger.info("接收到请求,参数param1:{},param2:{},param3:{},param4:{}", param1,param2,param3,param4);
+		
 		Integer result = param1 * param2 / param3 + param4;
+		
 		logger.info("处理请求,结果:{}", result);
 		return "frontend/index";	
 	}
