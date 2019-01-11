cbf6ed784ae2538c9d2e7cf7a8264a22f3440ee0
‘yinwenwei’ <1043936421@qq.com>
2019-01-11 16:48:19 +0800

    接口修改

--- SwimInWater-parent/SwimInWater-api/src/main/java/com/yyl/api/scenicarea/ScenicareaApi.java
+++ SwimInWater-parent/SwimInWater-api/src/main/java/com/yyl/api/scenicarea/ScenicareaApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.yyl.entity.Dictionary;
 import com.yyl.entity.Hotel;
+import com.yyl.entity.Line;
 import com.yyl.entity.Scenicspot;
 
 import io.swagger.annotations.Api;
@@ -22,11 +23,14 @@ public interface ScenicareaApi {
 	List<Scenicspot> findScenicspotAll();
 	//查询景点分区(dTypeClassification='scenic_Region')
 	List<Dictionary> findScenicRegion();
+	//根据景点名称或景点城市查询
+	List<Scenicspot> findScenicspotBySNameOrSCity(String sName,String sCity);
+	//根据路线名称查询路线信息
+	List<Line> findLine(String lName);
 	//查询酒店信息
 	List<Hotel> findHotelAll(); 
-	
-	
-	//多条件搜索
+	//多条件搜索(根据线路名称,景点价格)
+	List<Line> findLineLikeLNameOrSPrice(String lName,Integer sPrice);
 	
 	//查询路线
 	
