1f1655929e91efde4c12ae9e643273112649f2da
kewei_liao <kewei_liao@163.com>
2019-01-12 12:01:30 +0800

    完善评论接口

--- SwimInWater-parent/SwimInWater-comment/src/main/java/com/yyl/comment/CommentImpl.java
+++ SwimInWater-parent/SwimInWater-comment/src/main/java/com/yyl/comment/CommentImpl.java
@@ -1,10 +1,24 @@
 package com.yyl.comment;
 
-
-
-
+import java.util.List;
 import com.yyl.api.comment.CommentApi;
+import com.yyl.entity.Comment;
 public class CommentImpl implements CommentApi{
+
+	@Override
+	public Integer addComment(Comment comment) {
+		return null;
+	}
+
+	@Override
+	public List<Comment> queryComment(String sId) {
+		return null;
+	}
+
+	@Override
+	public Integer deleteComment(String id) {
+		return null;
+	}
 	
 
 }
